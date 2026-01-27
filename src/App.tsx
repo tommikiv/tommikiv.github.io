b21bbe2734c53430f97a966a65173282b857d777
Tommi Kivikangas <tonni.kivikangas@gmail.com>
2026-01-27 23:35:40 +0200

    App header & About + Portrait

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './routes/Home';
+import { Github, SunMoon } from 'lucide-react';
 
 function App() {
+  const [theme, setTheme] = React.useState('dark');
+
+  React.useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-header">
+        <h2 className="title">Tommi Kivikangas</h2>
+        <div className="nav-links">
+          <a href="#home">Home</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+          <button className="github-button"><Github size={20} />GitHub</button>
+          <button className="theme-button" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}><SunMoon size={20} /></button>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
